d0f42aa8b6c256feee47744c85aafe396af120d2
arifahmed4208 <arifahmed4208@gmail.com>
2024-07-16 21:56:43 +0530

    404 support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
@@ -11,15 +10,14 @@ function App() {
   return (
     <ChakraProvider>
       <AuthProvider>
-        <Router>
+        <BrowserRouter>
           <Routes>
             <Route path="/exampractice">
               <Route index element={<Login />}></Route>
               <Route path="dashboard" element={<Dashboard />} />
             </Route>
-            
           </Routes>
-        </Router>
+        </BrowserRouter>
       </AuthProvider>
     </ChakraProvider>
   );
